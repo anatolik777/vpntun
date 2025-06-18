9a33de691311cf60d7e408261a0ac3f07602f1e3
anatolik777 <146458770+anatolik777@users.noreply.github.com>
2025-06-18 15:26:49 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Убедитесь, что скрипт запущен с правами суперпользователя
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Пожалуйста, запустите этот скрипт с правами суперпользователя."
+    exit 1
+fi
+
+# Установка прав на выполнение для всех файлов в директории vpntun
+chmod +x *
+
+# Перемещение файлов в нужные директории
+mv amn-via-socks.service /etc/systemd/system/
+mv tun2socks /usr/local/bin
+mv amn-via-socks.sh /root
+mv ip.sh /root
+mv go.sh /root
+cd ..
+
+# Удаление директории vpntun
+rm -r vpntun/
+
+# Обновление конфигурации systemd
+systemctl daemon-reload
+
+# Включение и перезапуск сервиса
+systemctl enable amn-via-socks
+systemctl restart amn-via-socks
+
+echo "Настройка завершена."
